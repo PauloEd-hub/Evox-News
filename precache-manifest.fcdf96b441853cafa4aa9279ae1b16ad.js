c955e1441ae33ab8372e33d295927a3b2220ed4f
PauloEd-hub <paulo2002.cavalcante@hotmail.com>
2020-09-16 23:53:03 -0300

    Updates

---
+++ precache-manifest.fcdf96b441853cafa4aa9279ae1b16ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d1c039cd6b220e42c0baa51f38871d",
+    "url": "/Evox-News/index.html"
+  },
+  {
+    "revision": "83004e5772716bad4ab3",
+    "url": "/Evox-News/static/css/main.11ec1c27.chunk.css"
+  },
+  {
+    "revision": "be1140ce4a16272a9d98",
+    "url": "/Evox-News/static/js/2.b439b675.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Evox-News/static/js/2.b439b675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83004e5772716bad4ab3",
+    "url": "/Evox-News/static/js/main.33d25550.chunk.js"
+  },
+  {
+    "revision": "9d619e29148ba370fe1b",
+    "url": "/Evox-News/static/js/runtime-main.b21a8c33.js"
+  }
+]);
